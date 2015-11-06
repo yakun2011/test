2d4d6e6aca2e0470bee1d755bae48845081db37a
yakun <iando2000@mail.ru>
2015-11-06 12:09:49 +0500

    Изменили index.php

--- autoload.php
+++ autoload.php
@@ -6,6 +6,7 @@ function __autoload($className) {
         require_once __DIR__ . '/controllers/' . $className . '.php';
     } elseif (file_exists( __DIR__ . '/models/' . $className . '.php')) {
         require_once __DIR__ . '/models/' . $className . '.php';
+    } elseif (file_exists( __DIR__ . '/classes/' . $className . '.php')) {
+        require_once __DIR__ . '/classes/' . $className . '.php';
     }
-    
 }

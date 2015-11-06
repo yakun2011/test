f2fba8c33a3636a71905278cd64c3e7c8d0cc322
yakun <iando2000@mail.ru>
2015-11-06 18:12:09 +0500

    Изменили index.php

---
+++ classes/View.php
@@ -0,0 +1,22 @@
+<?php
+
+class View {
+        
+    protected $data = [];
+    
+    public function assign($name, $value) {
+        
+        $this->data[$name] = $value;
+    }
+    
+    public function display($template) {
+
+        foreach ($this->data as $key => $val) {
+            $$key = $val;
+        }
+
+        include __DIR__ . '/../views/' . $template;
+        
+    }
+
+}

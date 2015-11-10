3a42cd438c486dd3a88d3f2ecc3aff3e6bc3f13a
yakun <iando2000@mail.ru>
2015-11-10 17:57:08 +0500

    Изменили index.php

--- classes/DB.php
+++ classes/DB.php
@@ -1,30 +1,50 @@
 <?php
 
 class DB {
+    
+    private $dbh;
+    private $className = 'stdClass';
 
-    public function __construct(){
-        
-        mysql_connect('localhost', 'root', '');
-        mysql_select_db('test');
+    public function __construct() {
+
+        $dsn = 'mysql:dbname=test;host=localhost';
+        $this->dbh = new PDO($dsn, 'root', '');
     }
     
-    public function queryAll($sql, $className = 'stdClass') {
+    public function setClassName($className) {
+        $this->className = $className;
+    }
+    
+    public function query($sql, $params = []) {
         
-        $res = mysql_query($sql);
-        if ($res === false) {
-            return false;
-        }
-        
-        $ret = [];
-        while ($row = mysql_fetch_object($res, $className)) {
-            $ret[] = $row;
-        }
-        return $ret;
+        $sth = $this->dbh->prepare($sql);
+        $sth->execute($params);
+        return $sth->fetchAll(PDO::FETCH_CLASS, $this->className);
     }
 
-    public function queryOne($sql, $className = 'stdClass') {
+    public function execute($sql, $params = []) {
 
-        $res = $this->queryAll($sql, $className = 'stdClass');
-        return $res[0];
+        $sth = $this->dbh->prepare($sql);
+        return $sth->execute($params);
     }
+    
+//    public function queryAll($sql, $className = 'stdClass') {
+//
+//        $res = mysql_query($sql);
+//        if ($res === false) {
+//            return false;
+//        }
+//        
+//        $ret = [];
+//        while ($row = mysql_fetch_object($res, $className)) {
+//            $ret[] = $row;
+//        }
+//        return $ret;
+//    }
+//
+//    public function queryOne($sql, $className = 'stdClass') {
+//
+//        $res = $this->queryAll($sql, $className = 'stdClass');
+//        return $res[0];
+//    }
 }

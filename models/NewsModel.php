850defd340aee1c9a5003c5d1a0e9c194e9ff0d1
yakun <iando2000@mail.ru>
2015-11-10 17:57:50 +0500

    Изменили index.php

---
+++ models/NewsModel.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Class NewsModel
+ * @property $id
+ * @property $title
+ * @property $text
+ */
+class NewsModel extends AbstractModel {
+    
+    static protected $table = 'news';
+
+}

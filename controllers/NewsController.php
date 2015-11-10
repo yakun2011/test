a4d06af51c84b9ee5eaa1f29f8169a74babe25f9
yakun <iando2000@mail.ru>
2015-11-10 17:58:07 +0500

    Изменили index.php

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -3,13 +3,16 @@
 class NewsController {
     
     public function actionAll(){
-
-        $news = News::getAll();
-        $view = new View();
-//        $view->assign('items', $news);
-        $view->items = $news;
         
-        $view->display('news/all.php');
+        $article = new NewsModel();
+        $article->title = 'Привет';
+        $article->text = 'Привет, МИР!';
+        $article->insert();
+        
+//        $news = News::getAll();
+//        $view = new View();
+//        $view->items = $news;
+//        $view->display('news/all.php');
     }
 
     public function actionOne(){

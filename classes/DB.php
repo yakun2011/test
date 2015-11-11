d0d5a1a54751c190b642e1d0f52cafa1db8d6b18
yakun2011 <iando2000@mail.ru>
2015-11-11 17:59:39 +0500

    backup

--- classes/DB.php
+++ classes/DB.php
@@ -28,23 +28,7 @@ class DB {
         return $sth->execute($params);
     }
     
-//    public function queryAll($sql, $className = 'stdClass') {
-//
-//        $res = mysql_query($sql);
-//        if ($res === false) {
-//            return false;
-//        }
-//        
-//        $ret = [];
-//        while ($row = mysql_fetch_object($res, $className)) {
-//            $ret[] = $row;
-//        }
-//        return $ret;
-//    }
-//
-//    public function queryOne($sql, $className = 'stdClass') {
-//
-//        $res = $this->queryAll($sql, $className = 'stdClass');
-//        return $res[0];
-//    }
+    public  function lastInsertId() {
+        return $this->dbh->lastInsertId();
+    }
 }

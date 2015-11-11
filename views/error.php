73d181e9b13db93a54cb30659d8747b85ddf05c4
yakun2011 <iando2000@mail.ru>
2015-11-11 18:00:29 +0500

    backup

---
+++ views/error.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ошибка!</title>
+</head>
+<body>
+<div>
+    <h2>Ошибка!</h2>
+    <p><?php echo $error; ?></p>
+</div>
+
+</body>
+</html>

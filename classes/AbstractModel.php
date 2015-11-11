3f4009c23ed8289877adf2642247c8003f6179f8
yakun2011 <iando2000@mail.ru>
2015-11-11 17:58:41 +0500

    backup

--- classes/AbstractModel.php
+++ classes/AbstractModel.php
@@ -13,6 +13,10 @@ abstract class AbstractModel {
     public function __get($k) {
         return $this->data[$k];
     }
+    
+    public function __isset($k) {
+        return isset($this->data[$k]);
+    }
         
     public static function findAll() {
         
@@ -31,8 +35,26 @@ abstract class AbstractModel {
         $db->setClassName($class);
         return $db->query($sql, [':id'=>$id])[0];
     }
+        
+    public static function findByColumn($column, $value) { // Возвращает массив объектов с заданным значением свойства
+        $db = new DB();
+        $db->setClassName(get_called_class());
+        $sql = 'SELECT * FROM ' . static::$table . ' WHERE ' . $column . '=:value';
+        return $db->query($sql, [':value'=>$value]);
+    }
+
+    public static function findOneByColumn($column, $value) { // Возвращает объект с заданным значением свойства
+        $db = new DB();
+        $db->setClassName(get_called_class());
+        $sql = 'SELECT * FROM ' . static::$table . ' WHERE ' . $column . '=:value';
+        $res = $db->query($sql, [':value'=>$value])[0];
+        if (empty($res)){
+            throw new ModelException('Шэф! Всё пропало!');
+        }
+        return false;
+    }
     
-    public function insert() {
+    protected function insert() {
         $cols = array_keys($this->data);
         $ins = [];
         $data = [];
@@ -50,6 +72,35 @@ abstract class AbstractModel {
 //        var_dump($data);die;
         $db = new DB();
         $db->execute($sql, $data);
+        $this->id = $db->lastInsertId();
+    }
+    
+    protected function update() {
+        $cols = [];
+        $data = [];
+        foreach ( $this->data as $k => $v ) {
+            $data[':' . $k] = $v;
+            if ($k == 'id') {
+                continue;
+            }
+            $cols[] = $k . '=:' . $k;
+        }
+        $sql = '
+            UPDATE ' . static::$table . '
+            SET ' . implode(', ', $cols) . '
+            WHERE id=:id
+        ';
+
+        $db = new DB();
+        $db->execute($sql, $data);
+    }
+    
+    public function save() {
+        if ( !isset($this->id) ) {
+            $this->insert();
+        } else {
+            $this->update();
+        }
     }
     
     public function fill($data = []) { // метод собирает в массив данные из формы и с помощью этого массива сам установит нужные свойства объекта 

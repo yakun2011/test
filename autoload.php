ff1404c28832223ac377af6c821bcfbd33d6e510
yakun2011 <iando2000@mail.ru>
2015-11-13 13:24:54 +0500

    backup

--- autoload.php
+++ autoload.php
@@ -8,5 +8,17 @@ function __autoload($className) {
         require_once __DIR__ . '/models/' . $className . '.php';
     } elseif (file_exists( __DIR__ . '/classes/' . $className . '.php')) {
         require_once __DIR__ . '/classes/' . $className . '.php';
+    } else {
+        $classParts = explode('\\', $className);
+        $classParts[0] = __DIR__;
+        $path = implode(DIRECTORY_SEPARATOR, $classParts) . '.php';
+        if (file_exists($path)) {
+            require $path;
+        }
+//        var_dump($path);
+//        die;
+        
+        
     }
+    
 }
